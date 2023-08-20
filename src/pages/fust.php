44a62246d40e6fc4b5ca75e3de938d5d71cf1924
Daan Penning <d.l.penning@student.tue.nl>
2023-08-20 17:46:01 +0200

    Fixed admin page styling

--- src/pages/fust.php
+++ src/pages/fust.php
@@ -1,11 +1,14 @@
-<h1>FUST Admin Page</h1>
-<table class="form-table">
-    <tbody>
-        <tr>
-            <th><label>Registrations</label></th>
-            <td>
-                <a href="#" class="button button-primary">Go to page</a>
-            </td>
-        </tr>
-    </tbody>
-</table>
+
+<div class="wrap">
+    <h1>FUST Admin Page</h1>
+    <table class="form-table">
+        <tbody>
+            <tr>
+                <th><label>Registrations</label></th>
+                <td>
+                    <a href="#" class="button button-primary">Go to page</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
