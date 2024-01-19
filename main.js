f3bf27d8ddf0550c182497275deedbbe966eb0bb
JesseCY <jesse@codeyellow.nl>
2024-01-19 12:27:11 +0100

    Style member registration page

--- main.js
+++ main.js
@@ -1,24 +1,53 @@
-const body = document.querySelector('body')
-const navScreen = document.querySelector('.nav-screen')
-const navBurger = document.querySelector('.nav-burger')
-const navClose = document.querySelector('.nav-close')
+/**
+ * Set up event listeners for the mobile navigation screen
+ */
+function setupNavigation() {
+  const body = document.querySelector('body')
+  const navScreen = document.querySelector('.nav-screen')
+  const navBurger = document.querySelector('.nav-burger')
+  const navClose = document.querySelector('.nav-close')
 
-if (navBurger) {
-  navBurger.addEventListener('click', () => {
-    openNav()
-  })
+  if (navBurger) {
+    navBurger.addEventListener('click', () => {
+      openNav()
+    })
+  }
+
+  function openNav() {
+    body.classList.add('overflow-hidden')
+    navScreen.classList.add('open')
+
+    navClose.addEventListener('click', () => {
+      closeNav()
+    })
+  }
+
+  function closeNav() {
+    body.classList.remove('overflow-hidden')
+    navScreen.classList.remove('open')
+  }
 }
 
-function openNav() {
-  body.classList.add('overflow-hidden')
-  navScreen.classList.add('open')
+setupNavigation()
 
-  navClose.addEventListener('click', () => {
-    closeNav()
-  })
+
+/**
+ * Set up event listeners for members sign-up page
+ */
+function setupMemberSignup() {
+  const wpcf7Form = document.querySelector('.wpcf7-form')
+  const institutionSelect = document.querySelector('[name="institution"]')
+
+
+  if (institutionSelect) {
+    institutionSelect.addEventListener('change', (e) => {
+      if (e.target.value === 'No study in Tilburg') {
+        wpcf7Form.setAttribute('data-institution-else-visible', 'true')
+      } else {
+        wpcf7Form.setAttribute('data-institution-else-visible', 'false')
+      }
+    })
+  }
 }
 
-function closeNav() {
-  body.classList.remove('overflow-hidden')
-  navScreen.classList.remove('open')
-}
+setupMemberSignup()
