2be80b26ace108f8b68b686b4d5a6941c39d2722
JesseCY <jesse@codeyellow.nl>
2024-01-19 12:25:39 +0100

    Add full FontAwesome support

--- templates/head.php
+++ templates/head.php
@@ -9,6 +9,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,500;0,700;1,400&family=League+Spartan:wght@400;500;600&display=swap" rel="stylesheet">
+    
+    <link href="<?= get_template_directory_uri() ?>/assets/fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="<?= get_template_directory_uri() ?>/assets/fontawesome/css/brands.css" rel="stylesheet">
+    <link href="<?= get_template_directory_uri() ?>/assets/fontawesome/css/solid.css" rel="stylesheet">
+    <link href="<?= get_template_directory_uri() ?>/assets/fontawesome/css/regular.css" rel="stylesheet">
 </head>
 <body>
     <div class="body-inner">
