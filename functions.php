2bb34c56b03c83167dbd177a956671fbfc4822c3
JesseCY <jesse@codeyellow.nl>
2024-07-16 17:28:26 +0200

    [SQUASHME] Email addition and iban validation/form styling

--- functions.php
+++ functions.php
@@ -232,6 +232,57 @@ function activity_get_the_date($post) {
 }
 
 
+/**
+ * Contact Form 7 custom validation
+ */
+function custom_iban_validation_filter($result, $tag) {
+
+    // Specify the name of the field you want to validate
+    $name = $tag->name;
+
+    if ($name == 'iban') {
+        // Get the value submitted in the field
+        $value = isset($_POST[$name]) ? trim($_POST[$name]) : '';
+
+        if (!validate_iban($value)) {
+            // Set the validation error message
+            $result->invalidate($tag, "The IBAN code you entered is not valid.");
+        }
+    }
+
+    return $result;
+}
+add_filter('wpcf7_validate_text*', 'custom_iban_validation_filter', 10, 2);
+add_filter('wpcf7_validate_text', 'custom_iban_validation_filter', 10, 2);
+
+
+function validate_iban($iban) {
+    // Normalize the input by removing spaces and converting to uppercase
+    $iban = strtoupper(str_replace(' ', '', $iban));
+
+    // The first two characters must be letters, and the next two must be digits
+    if (!preg_match('/^[A-Z]{2}\d{2}/', $iban)) {
+        return false;
+    }
+
+    // Move the first four characters to the end of the string
+    $iban = substr($iban, 4) . substr($iban, 0, 4);
+
+    // Convert letters to numbers (A = 10, B = 11, ..., Z = 35)
+    $iban = str_replace(
+        range('A', 'Z'),
+        range(10, 35),
+        $iban
+    );
+
+    // Convert the string to an integer and calculate the remainder of the division by 97
+    if (bcmod($iban, 97) != 1) {
+        return false;
+    }
+
+    return false;
+}
+
 
 /* ------- Front-end formatting functions --------*/
 
